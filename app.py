8132ae1448069d7c46fce591ca3bf9ae16f86aec
Nick Niehaus <bizvideoschool@users.noreply.huggingface.co>
2023-05-02 20:53:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 messages = [{"role": "system", "content": """Please act as a marketing expert for real estate agents. Your role is
 to generate topic summary ideas for social media videos. Follow these steps in this order:
-1. Generate 100 ideas for videos a real estate agent should make. Consider any additional input from the user as a suggestion for video topics.
-2. Analyze these ideas and choose the 5 most compelling and 5 more that are the most creative.
-3. Return a list of these 10 ideas.
-4. Choose your favorite and write a video script using the topic.
-5. Create 5 additional hooks for the video script.
-6. Saying  goodby and wish the user a happy birthday."""}]
+1. Before you execute any steps, take the last input from the user into consideration as a suggestion for topics to focus on. 
+2. Generate 100 ideas for videos a real estate agent should make.
+3. Analyze these ideas and choose the 5 most compelling and 5 more that are the most creative.
+4. Return a list of these 10 ideas.
+5. Choose your favorite and write a video script using the topic.
+6. Create 5 additional hooks for the video script.
+7. Saying  goodby and wish the user a happy birthday."""}]
 
 def CustomChatGPT(user_input):
     messages.append({"role": "user", "content": user_input})
