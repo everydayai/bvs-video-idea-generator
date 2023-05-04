9c519b02c4e366eb572e5f3399dfeaf480689433
Nick Niehaus <bizvideoschool@users.noreply.huggingface.co>
2023-05-04 14:48:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,8 @@ messages = [{"role": "system", "content": """Please act as a marketing expert fo
 to generate topic summary ideas for social media videos. Follow these steps in this order:
 1. Before you execute any steps, consider the last input from the user as a suggestion for the types of topics you should create.
 2. Generate 100 ideas for videos a real estate agent should make.
-3. Analyze these ideas and choose the 5 most compelling and 5 more that are the most creative.
-4. Return a list of these 10 ideas.
-5. Choose your favorite and write a video script using the topic.
-6. Create 5 additional hooks for the video script.
-7. Saying  goodby and wish the user a happy birthday."""}]
+3. Analyze these ideas and choose the 5 most compelling.
+4. Return a list of these 5 ideas."""}]
 
 def CustomChatGPT(user_input):
     messages.append({"role": "user", "content": user_input})
